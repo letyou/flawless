55e74dd86da22d9c1827bfc2703479d6489502bb
0216刘剑全 <43239916+liujianquan0216@users.noreply.github.com>
2018-09-15 14:37:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 #define s 0x12345678
 #define MAX(x,y) ( ((x) > (y)) ? (x) : (y) )//最大值
 #define MIN(x,y) ( ((x) < (y)) ? (x) : (y) )//最小值
-typedef quint32 word;
-typedef quint8  byte;
+typedef qint32 word;
+typedef qint8  byte;
 #define LLO(xxx) ((byte) ((word)(xxx) & 255)) //最低位
 #define HLO(xxx) ((byte) ((word)(xxx) >> 8))  //次低位
 #define LHI(xxx) ((byte) ((word)(xxx) >> 16)) //次高位
@@ -17,22 +17,22 @@ typedef quint8  byte;
 
 int main()
 {
-    QList<byte> value;
-    value<<HHI(s)<<LHI(s)<<HLO(s)<<LLO(s);
+    QList<byte> values;
+    values<<HHI(s)<<LHI(s)<<HLO(s)<<LLO(s);
 
     word z;
     z=HLO(s)<<24|HHI(s)<<16|LLO(s)<<8|LHI(s);
 
-    qDebug()<<"原始值：0x12345678=="<<s;
+    qDebug()<<"原始值：0x12345678 =="<<s;
 
-    qDebug()<<"0x12=="<<value.at(0)<<"0x34=="<<value.at(1)<<"0x56=="<<value.at(2)<<"0x78=="<<value.at(3);
+    qDebug()<<"0x12 =="<<values.at(0)<<"0x34 =="<<values.at(1)<<"0x56 =="<<values.at(2)<<"0x78 =="<<values.at(3);
 
     qDebug("最高8位和次高8位最大值：0x%x(%d)",MAX(HHI(s),LHI(s)),MAX(HHI(s),LHI(s)));
     qDebug("次低8位和最低8位最小值：0x%x(%d)",MIN(HLO(s),LLO(s)),MIN(HLO(s),LLO(s)));
 
     qDebug("重新组合后数值：0x%x(%d)",z,z);
-    std::sort(value.begin(),value.end(),std::less<byte>());//升序
-    qDebug()<<value;
-    std::sort(value.begin(),value.end(),std::greater<byte>());//降序
-    qDebug()<<value;
+    std::sort(values.begin(),values.end(),std::less<byte>());//升序
+    qDebug()<<values;
+    std::sort(values.begin(),values.end(),std::greater<byte>());//降序
+    qDebug()<<values;
 }
