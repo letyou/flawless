14b2cf67b5cfbe0bed7bd99a0d146435bed98058
0216刘剑全 <43239916+liujianquan0216@users.noreply.github.com>
2018-09-15 14:05:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include <vector>
 
 #define s 0x12345678
-#define MAX(x,y) ( ((x) > (y)) ? (x) : (y) )
-#define MIN(x,y) ( ((x) < (y)) ? (x) : (y) )
+#define MAX(x,y) ( ((x) > (y)) ? (x) : (y) )//最大值
+#define MIN(x,y) ( ((x) < (y)) ? (x) : (y) )//最小值
 typedef quint32 word;
 typedef quint8  byte;
 #define LLO(xxx) ((byte) ((word)(xxx) & 255)) //最低位
@@ -17,22 +17,22 @@ typedef quint8  byte;
 
 int main()
 {
-    QList<byte> v;
-    v<<HHI(s)<<LHI(s)<<HLO(s)<<LLO(s);
+    QList<byte> value;
+    value<<HHI(s)<<LHI(s)<<HLO(s)<<LLO(s);
 
     word z;
     z=HLO(s)<<24|HHI(s)<<16|LLO(s)<<8|LHI(s);
 
     qDebug()<<"原始值：0x12345678=="<<s;
 
-    qDebug()<<"0x12=="<<v.at(0)<<"0x34=="<<v.at(1)<<"0x56=="<<v.at(2)<<"0x78=="<<v.at(3);
+    qDebug()<<"0x12=="<<value.at(0)<<"0x34=="<<value.at(1)<<"0x56=="<<value.at(2)<<"0x78=="<<value.at(3);
 
     qDebug("最高8位和次高8位最大值：0x%x(%d)",MAX(HHI(s),LHI(s)),MAX(HHI(s),LHI(s)));
     qDebug("次低8位和最低8位最小值：0x%x(%d)",MIN(HLO(s),LLO(s)),MIN(HLO(s),LLO(s)));
 
     qDebug("重新组合后数值：0x%x(%d)",z,z);
-    std::sort(v.begin(),v.end(),std::less<byte>());
-    qDebug()<<v;
-    std::sort(v.begin(),v.end(),std::greater<byte>());
-    qDebug()<<v;
+    std::sort(value.begin(),value.end(),std::less<byte>());
+    qDebug()<<value;
+    std::sort(value.begin(),value.end(),std::greater<byte>());
+    qDebug()<<value;
 }
